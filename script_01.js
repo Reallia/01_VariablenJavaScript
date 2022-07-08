1e4c937c887da1bc347e48ba9dbb497cadbb36a9
BaeckerAnna <baecker.anna2@gmail.com>
2022-07-08 09:16:13 +0200

    /***** 02 Deklaration + Wertzuweisung *******/

--- script_01.js
+++ script_01.js
@@ -13,3 +13,11 @@
 // console.log(firstName);
 
 /***** 02 Deklaration + Wertzuweisung *******/
+
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); //Ausgabe
+
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log(familyName); //Ausgabe
+console.log(firstName + " " + familyName); //
